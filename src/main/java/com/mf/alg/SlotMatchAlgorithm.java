85fb92dcb42408d0952f9fdb6c8f69c38eb88e9e
冯志勇 <smcoder@fengzhiyongs-MacBook-Pro.local>
2022-04-28 01:05:35 +0800

    feature:算法实现

---
+++ src/main/java/com/mf/alg/SlotMatchAlgorithm.java
@@ -0,0 +1,51 @@
+package com.mf.alg;
+
+public class SlotMatchAlgorithm {
+    int[][] position;
+    int[] matched;
+    int length;
+    boolean[] accessed;
+
+    public SlotMatchAlgorithm(int[][] position) {
+        this.position = position;
+        length = this.position.length;
+        accessed = new boolean[length];
+
+        matched = new int[length];
+        for (int i = 0; i < length; i++) {
+            matched[i] = -1;
+            accessed[i] = false;
+        }
+    }
+
+    public boolean findAugmentPath(int value) {
+        for (int i = 0; i < length; i++) {
+            if (position[value][i] == 1) {
+                if (!accessed[i]) {
+                    accessed[i] = true;
+                    if (matched[i] == -1 || findAugmentPath(matched[i])) {
+                        matched[value] = i;
+                        matched[i] = value;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    void search() {
+        for (int i = 0; i < length; i++) {
+            if (matched[i] == -1) {
+                clearAccessed();
+                findAugmentPath(i);
+            }
+        }
+    }
+
+    void clearAccessed() {
+        for (int i = 0; i < length; i++) {
+            accessed[i] = false;
+        }
+    }
+}
